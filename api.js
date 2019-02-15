bcacdb45e9b5f7f017371840500eb8b88e4c36f6
Oskar Vatic <oskarsmit@hotmail.com>
2019-02-15 19:37:39 +0100

    Add bunch of endpoints

--- api.js
+++ api.js
@@ -1,18 +1,21 @@
 'use strict'
 
 const Api = require('claudia-api-builder');
+const getPizzas = require('./handlers/get-pizzas')
 
 const api = new Api();
 
-api.get('/pizzas', () => {
-    return [
-        'Capricciosa',
-        'Quattro Formaggi',
-        'Napoletana',
-        'Margherita',
-    ]
-});
+api.get('/', () => 'This is the base API page, please specify route');
 
 api.get('/oskar', () => 'Oskar');
+
+api.get('/pizzas', () => getPizzas());
+
+api.get('/pizzas/{id}',
+        request => getPizzas(request.pathParams.id),
+    {
+        error: 404
+    }
+)
 
 module.exports = api;
